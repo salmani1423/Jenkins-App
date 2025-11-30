694d053690ca2a4a55193f2529f25a20d109b7ef
Nasir Salmani <nasir.salmani@sabpaisa.in>
2025-11-30 20:19:12 +0530

    fixed endpoint

--- src/main/java/com/example/demo/controller/DemoController.java
+++ src/main/java/com/example/demo/controller/DemoController.java
@@ -1,13 +1,13 @@
 package com.example.demo.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DemoController {
 	
 	
-	@RequestMapping(name = "/send-data")
+	@GetMapping("/send-data")
 	public String sendData() {
 		
 		return "welcome";
